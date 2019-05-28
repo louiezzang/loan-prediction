efa8168043f0862b621c93acaff377a66ffbe5c6
louie <louie@yojee.com>
2019-05-29 01:55:42 +0800

    Add data preprocessing and logistic regression model

--- prediction_model.py
+++ prediction_model.py
@@ -4,16 +4,21 @@ author: Younggue Bae
 
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
 
 class PredictionModel(object):
 
-    def __init__(self, debug=True):
+    def __init__(self, solver='liblinear', penalty='l2', max_iter=100, debug=True):
         """
         Constructor.
+        :param solver: {‘newton-cg’, ‘lbfgs’, ‘liblinear’, ‘sag’, ‘saga’}, optional (default=’liblinear’)
+        :param penalty: ‘l1’, ‘l2’, ‘elasticnet’ or ‘none’, optional (default=’l2’)
+        :param max_iter: Maximum number of iterations taken for the solvers to converge (default=100)
         :param debug:
         """
         self.debug = debug
+        self.lr = LogisticRegression(solver=solver, penalty=penalty, C=1000.0, max_iter=max_iter, random_state=0)
 
     def fit(self, X, y):
         """
@@ -24,6 +29,7 @@ class PredictionModel(object):
                   Ground truth labels as a numpy array of 0-s and 1-s.
         :return: None
         """
+        return self.lr.fit(X, y)
 
     def predict(self, X):
         """
@@ -33,6 +39,7 @@ class PredictionModel(object):
         :return: np.ndarray
                  eg. np.array([1, 0, 1])
         """
+        return self.predict(X)
 
     def predict_proba(self, X):
         """
@@ -42,6 +49,7 @@ class PredictionModel(object):
         :return: np.ndarray
                  eg. np.array([[0.2, 0.8], [0.9, 0.1], [0.5, 0.5]])
         """
+        return self.predict_proba(X)
 
     def evaluate(self, X, y):
         """
